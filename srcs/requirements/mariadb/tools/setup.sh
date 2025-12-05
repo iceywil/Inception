42bedcb12854fb0aff77f17e1907f98feb1fef12
Wilfried Scherrer <wscherre@f2r9s18.clusters.42paris.fr>
2025-12-05 20:58:00 +0100

    fix

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -21,11 +21,11 @@ else
     done
 
     # Run setup commands. The initial root user has no password.
-    mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-    mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' WITH GRANT OPTION;"
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-    mysql -e "FLUSH PRIVILEGES;"
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+    mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' WITH GRANT OPTION;"
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
 
     # Shutdown the temporary server using the newly set root password.
     if ! mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown; then
